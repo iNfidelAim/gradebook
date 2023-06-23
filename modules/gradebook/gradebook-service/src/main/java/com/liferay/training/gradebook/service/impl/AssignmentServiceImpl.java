48c5b6d4fdde5bf97c60d387083d077637fac513
rafanasiev <rafanasiev@emdev.ru>
2023-06-23 15:18:04 +0700

    Implement Assignment Remote Service
    1. Implement the new dependencies as follows.
    
    2. Implement the Façade Methods in AssignmentServiceImpl.java class as follows

--- modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentServiceImpl.java
+++ modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentServiceImpl.java
@@ -15,9 +15,19 @@
 package com.liferay.training.gradebook.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.training.gradebook.model.Assignment;
 import com.liferay.training.gradebook.service.base.AssignmentServiceBaseImpl;
 
 import org.osgi.service.component.annotations.Component;
+
+import java.rmi.Remote;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Ruslan
@@ -30,4 +40,47 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class AssignmentServiceImpl extends AssignmentServiceBaseImpl {
+
+	public Assignment addAssignment(long groupId, Map<Locale, String> titleMap, Map<Locale, String> descriptionMap,
+									Date dueDate, ServiceContext serviceContext)
+
+			throws PortalException {
+		return assignmentLocalService.addAssignment(groupId, titleMap, descriptionMap, dueDate, serviceContext);
+	}
+	public Assignment deleteAssignment(long assignmentId)
+			throws PortalException {
+		Assignment assignment =
+				assignmentLocalService.getAssignment(assignmentId);
+		return assignmentLocalService.deleteAssignment(assignment);
+	}
+	public Assignment getAssignment(long assignmentId)
+			throws PortalException {
+		Assignment assignment =
+				assignmentLocalService.getAssignment(assignmentId);
+		return assignment;
+	}
+	public List<Assignment> getAssignmentsByGroupId(long groupId)
+	{
+		return assignmentPersistence.findByGroupId(groupId);
+	}
+	public List<Assignment> getAssignmentsByKeywords(
+			long groupId, String keywords, int start, int end,
+			OrderByComparator<Assignment> orderByComparator) {
+		return assignmentLocalService.getAssignmentsByKeywords(
+				groupId, keywords, start, end, orderByComparator);
+	}
+	public long getAssignmentsCountByKeywords(long groupId, String keywords) {
+		return assignmentLocalService.getAssignmentsCountByKeywords(
+				groupId, keywords);
+	}
+	public Assignment updateAssignment(
+			long assignmentId, Map<Locale, String> titleMap, Map<Locale, String> descriptionMap,
+			Date dueDate, ServiceContext serviceContext)
+			throws PortalException {
+
+		return assignmentLocalService.updateAssignment(
+				assignmentId, titleMap, descriptionMap, dueDate, serviceContext);
+	}
+
+
 }
