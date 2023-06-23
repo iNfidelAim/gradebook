6d526cecb439cdd4aaa1eecf15e7c59be04da04e
rafanasiev <rafanasiev@emdev.ru>
2023-06-23 17:45:22 +0700

    Create the Gradebook Web Module: 1. Dependencies
    2. To the GradebookPortlet class implement the changes to component properties as follows.
    
    3.  src/main/resources/content/Language.properties implement the contents as follows.
    
    P.S. Сделал портлет который можно увидеть и добавить через виджеты.

---
+++ modules/gradebook/gradebook-web/src/main/java/com/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.training.gradebook.web.constants;
+
+/**
+ * @author rusla
+ */
+public class GradebookPortletKeys {
+
+	public static final String GRADEBOOK =
+		"com_training_gradebook_web_portlet_GradebookPortlet";
+
+}
---
+++ modules/gradebook/gradebook-web/src/main/java/com/training/gradebook/web/portlet/GradebookPortlet.java
@@ -0,0 +1,32 @@
+package com.training.gradebook.web.portlet;
+
+import com.training.gradebook.web.constants.GradebookPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author rusla
+ */
+@Component(
+	property = {
+			"com.liferay.portlet.display-category=category.sample",
+			"com.liferay.portlet.header-portlet-css=/css/main.css",
+			"com.liferay.portlet.instanceable=true",
+			"javax.portlet.display-name=Gradebook",
+			"javax.portlet.init-param.template-path=/",
+			"javax.portlet.init-param.view-template=/view.jsp",
+			"javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
+			"javax.portlet.resource-bundle=content.Language",
+			"javax.portlet.security-role-ref=power-user,user",
+			"com.liferay.portlet.display-category=category.training",
+			"com.liferay.portlet.instanceable=false",
+
+	},
+	service = Portlet.class
+)
+public class GradebookPortlet extends MVCPortlet {
+}
