eff4b0c94b9910f5f974b15e89a8f6099085de7a
rafanasiev <rafanasiev@emdev.ru>
2023-06-26 16:30:25 +0700

    Implement Service Module Permissions
    1. Create the Resource Constants Class
    To avoid misspellings in permission properties, we'll create a constants class in the
    gradebook-api module.
    - Create a class
    com.liferay.training.gradebook.constants.GradebookConstants and
    implement as follows: ```java package com.liferay.training.gradebook.constants
    
    2.Define the Permissions
    By default, permissions are defined in the file called default.xml .
    - Create a folder src/main/resources/resource-actions in the gradebookservice module.
    - Create a file src/main/resources/resource-actions/default.xml and
    implement as follows (switch to Source mode, if needed)
    
    3. Define the Permissions Definition Location
    - Create a file src/main/resources/portlet.properties in the gradebookservice module.
    - Implement the file as follows:
    resource.actions.configs=/resource-actions/default.xml
    
    4. Implement Permission Resource Management
    Permissions need container objects for the model entities. When we create an entity, we
    need to create and bind a resource object to that. Accordingly, we have to take care of
    cleaning up the resources when we delete the entity:
    - In the class com.liferay.training.gradebook.service.impl.AssignmentLocalServiceImpl and replace
    the addAssignment() method with the following. Notice the highlighted.
    
    - Implement a new signature for deleting assignments
    
    5. Create the Permission Registrar Classes
    Create classes for registering the model and top-level resource permissions:
    
    - Create a class com.liferay.training.gradebook.internal.security.permission.resource.definition.AssignmentModelResourcePermissionDefinition
    in the gradebook-service module
    
    - Create a class for registering the Gradebook portlet resources and top level permissions:
    com.liferay.training.gradebook.internal.security.permission.resource.definition.AssignmentPortletResourcePermissionDefinition
    
    - Implement Permission Checking in the Remote Service
    Now let's implement the permission checking in our remote service class. Let's go through
    the implementation steps first.
    
    - Then we'll implement permission checking in addAssignment(), deleteAssignment() and updateAssignment() methods.
    
    - FindBy finder calls are transformed into Filtered Finder queries, which take permissions into account:
    assignmentPersistence.filterFindByGroupId(groupId);
    
    - In this exercise, we'll allow everybody to see and search the assignments list by the
    keyword search method getAssignmentsByKeywords . We just don't allow
    unauthorized users to view, update or delete them. w implement the class
    com.liferay.training.gradebook.service.impl.AssignmentServiceImpl.ja
    va as follows. You'll see an error for the filterFindBy method because it's not yet
    generated.
    
    6. Start servicebuilder

--- modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentLocalServiceImpl.java
+++ modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentLocalServiceImpl.java
@@ -3,6 +3,7 @@ package com.liferay.training.gradebook.service.impl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.training.gradebook.model.Assignment;
@@ -37,36 +38,48 @@ public class AssignmentLocalServiceImpl extends AssignmentLocalServiceBaseImpl {
 			long groupId, Map<Locale, String> titleMap, Map<Locale, String> descriptionMap,
 			Date dueDate, ServiceContext serviceContext)
 			throws PortalException {
-
-			_assignmentValidator.validate(titleMap, descriptionMap, dueDate);
-
+// Validate assignment parameters.
+		_assignmentValidator.validate(titleMap, descriptionMap, dueDate);
 // Get group and user.
-
 		Group group = groupLocalService.getGroup(groupId);
 		long userId = serviceContext.getUserId();
 		User user = userLocalService.getUser(userId);
-
 // Generate primary key for the assignment.
-
 		long assignmentId =
 				counterLocalService.increment(Assignment.class.getName());
-
 // Create assigment. This doesn't yet persist the entity.
-
 		Assignment assignment = createAssignment(assignmentId);
-
 // Populate fields.
 		assignment.setCompanyId(group.getCompanyId());
-		assignment.setCreateDate(serviceContext.getCreateDate(new Date()));
+		assignment.setCreateDate(serviceContext.getCreateDate(new
+				Date()));
 		assignment.setDueDate(dueDate);
 		assignment.setDescriptionMap(descriptionMap);
 		assignment.setGroupId(groupId);
-		assignment.setModifiedDate(serviceContext.getModifiedDate(new Date()));
+		assignment.setModifiedDate(serviceContext.getModifiedDate(
+				new Date()));
 		assignment.setTitleMap(titleMap);
 		assignment.setUserId(userId);
 		assignment.setUserName(user.getScreenName());
 // Persist assignment to database.
-		return super.addAssignment(assignment);
+		assignment = super.addAssignment(assignment);
+// Add permission resources.
+		boolean portletActions = false;
+		boolean addGroupPermissions = true;
+		boolean addGuestPermissions = true;
+		resourceLocalService.addResources(
+				group.getCompanyId(), groupId, userId, Assignment.class.getName(),
+				assignment.getAssignmentId(), portletActions, addGroupPermissions,
+				addGuestPermissions);
+
+		return assignment;
+	}
+
+	public Assignment deleteAssignment(Assignment assignment) throws PortalException {
+// Delete permission resources.
+		resourceLocalService.deleteResource(assignment, ResourceConstants.SCOPE_INDIVIDUAL);
+// Delete the Assignment
+		return super.deleteAssignment(assignment);
 	}
 
 	public Assignment updateAssignment(long assignmentId, Map<Locale, String> titleMap, Map<Locale,
